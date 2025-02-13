b7fcf137019720c64e5826b899edb19fab67d6c0
Beyond-Repair <132061760+beyond-repair@users.noreply.github.com>
2025-02-13 13:33:24 -0600

    Update

---
+++ AtomicNexusAI/config/logging_config.py
@@ -0,0 +1,26 @@
+import logging
+import logging.config
+
+LOGGING_CONFIG = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+        },
+    },
+    'handlers': {
+        'default': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard'
+        },
+    },
+    'root': {
+        'handlers': ['default'],
+        'level': 'INFO',
+    },
+}
+
+def setup_logging():
+    logging.config.dictConfig(LOGGING_CONFIG)
