4c2498c097070fbf553e921ca3b5b6196af85ed0
Beyond-Repair <132061760+beyond-repair@users.noreply.github.com>
2025-02-13 13:00:36 -0600

    Update model_manager.py

--- AtomicNexusAI/core/models/model_manager.py
+++ AtomicNexusAI/core/models/model_manager.py
@@ -1,11 +1,11 @@
-model_manager.py
+# model_manager.py
 
 class ModelManager:
-    def __init__(self):
+    def __init__(self) -> None:
         self.models = {}
 
-    def register_model(self, name, model):
+    def register_model(self, name: str, model: object) -> None:
         self.models[name] = model
 
-    def get_model(self, name):
+    def get_model(self, name: str) -> object | None:
         return self.models.get(name)
