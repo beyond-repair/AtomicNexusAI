3d23fe255b93e6eeac4830e4fe26cbe7b0970cd1
Beyond-Repair <132061760+beyond-repair@users.noreply.github.com>
2025-02-13 13:17:24 -0600

    Update predictive_cache.py

--- AtomicNexusAI/utils/optimizers/predictive_cache.py
+++ AtomicNexusAI/utils/optimizers/predictive_cache.py
@@ -1,10 +1,10 @@
-predictive_cache.py
+# predictive_cache.py
 
 class PredictiveCache:
-    def __init__(self):
+    def __init__(self) -> None:
         self.cache = {}
 
-    def predict_and_cache(self, key, compute_func):
+    def predict_and_cache(self, key: str, compute_func: callable) -> any:
         if key not in self.cache:
             self.cache[key] = compute_func()
         return self.cache[key]
