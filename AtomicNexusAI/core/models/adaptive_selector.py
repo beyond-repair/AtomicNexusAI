f97e1fd8e00dcbac6236b9d3a7b32889be7dfecf
Beyond-Repair <132061760+beyond-repair@users.noreply.github.com>
2025-02-13 12:05:40 -0600

    Update

---
+++ AtomicNexusAI/core/models/adaptive_selector.py
@@ -0,0 +1,9 @@
+adaptive_selector.py
+
+class AdaptiveSelector:
+    def __init__(self):
+        pass
+
+    def select_model(self, task):
+        # Placeholder logic: always returns 'default_model'
+        return "default_model"
