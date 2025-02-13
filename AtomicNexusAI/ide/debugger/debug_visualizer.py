22dc5726f8c67d2d559dd1fb554519f5e51ef754
Beyond-Repair <132061760+beyond-repair@users.noreply.github.com>
2025-02-13 13:06:10 -0600

    Update debug_visualizer.py

--- AtomicNexusAI/ide/debugger/debug_visualizer.py
+++ AtomicNexusAI/ide/debugger/debug_visualizer.py
@@ -1,8 +1,8 @@
-debug_visualizer.py
+# debug_visualizer.py
 import plotly.express as px
 
 class DebugVisualizer:
-    def visualize(self, agent_state):
+    def visualize(self, agent_state: object) -> None:
         fig = px.sunburst(
             agent_state.decision_tree,
             path=['task', 'model_used'],
