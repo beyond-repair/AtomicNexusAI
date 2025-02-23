9081f8e23c74171f499b5c7124faa7a859fdd649
Beyond-Repair <132061760+beyond-repair@users.noreply.github.com>
2025-02-23 17:47:40 -0600

    Update

---
+++ utils/logging/performance_tracer.py
@@ -0,0 +1,9 @@
+import time
+
+class PerformanceTracer:
+    def trace(self, func, *args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(f"Function {func.__name__} executed in {end - start:.4f} seconds")
+        return result
